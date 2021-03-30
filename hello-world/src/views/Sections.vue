fbfa96af74f6aa851b5cbc0092b7fe0018ee605e
PolyAtomicIon <180107265@stu.sdu.edu.kz>
2021-03-30 15:45:13 +0600

    d

---
+++ hello-world/src/views/Sections.vue
@@ -0,0 +1,43 @@
+<template> 
+
+  <div class="wrapper">
+
+    <section-header />
+
+    <sections-list />
+
+    <div class="item"><sections-list itemName="Basketball" /></div>
+    <div class="item"><sections-list itemName="Voleyball" /></div>
+    <div class="item"><sections-list itemName="Tennis" /></div>
+    <div class="item"><sections-list itemName="Swimming" /></div>
+    
+  </div>
+
+</template>
+
+<script>
+import SectionHeader from '../components/headers/SectionHeader.vue'
+import SectionsList from '../components/items/SectionsList.vue'
+
+export default {
+  mounted(){
+
+    console.log( this.$route.query.id );
+  },
+
+  components: {
+    SectionHeader,
+    SectionsList
+  },
+}
+</script>
+
+<style scoped>
+  .wrapper {
+    background:#C0C0C0;
+  }
+  
+  .item {
+    margin-top: 25px;
+  }
+</style>
