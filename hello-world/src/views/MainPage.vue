43901c11f9f3b1a1a693b121aa197611aa9a1fa6
daryn-coder <daryn-coder@users.noreply.github.com>
2021-03-30 11:51:20 +0600

    changes

--- hello-world/src/views/MainPage.vue
+++ hello-world/src/views/MainPage.vue
@@ -2,16 +2,20 @@
   <div class="wrapper">
   
     <main-page-header />
+    <activities-page/>
 
   </div>
 </template>
 
 <script>
 import MainPageHeader from '../components/MainPageHeader.vue'
+import ActivitiesPage from '../components/ActivitiesPage.vue'
+
 
 export default {
   components: {
     MainPageHeader,
+    ActivitiesPage
   }
 }
 </script>
---
+++ hello-world/src/views/ThirdPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <information-page/>
+  </div>
+</template>
+
+<script>
+import InformationPage from '../components/InformationPage.vue'
+
+export default {
+  
+  components: {
+    InformationPage
+  }
+}
+</script>
+
+<style>
+
+</style>
